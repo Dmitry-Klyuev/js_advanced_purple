cfca9830bd60adcb56006daa77690dba5ff1e209
Dmitry-Klyuev <i6169696@gmail.com>
2024-10-16 21:35:11 +0300

    init map and set

---
+++ Maps и Sets/main.js
@@ -0,0 +1,30 @@
+// Sets
+const flights = ['Russia', 'USA', 'London', 'Japan', 'London', 'USA'];
+
+
+//Уникализирукм массив
+const setFlights = new Set(flights); // Set(4) { 'Russia', 'USA', 'London', 'Japan' }
+
+// узнать размер
+setFlights.size // 4
+
+// проверка на наличие элемента
+setFlights.has('Russia') // true
+setFlights.has('USA') // true
+setFlights.has('England') // false
+
+// удаление элемента
+setFlights.delete('Russia')
+setFlights.has('Russia') // false
+
+// добавление элемента
+setFlights.add('Russia')
+setFlights.has('Russia') // true
+
+// очистка
+setFlights.clear() // Set(0) {}
+
+// преобразование в массив
+const arrFlights = [...setFlights]
+
+console.log([...new Set('abcdda')].join(''));
